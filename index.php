feb8d7c0afbdb8fc0393d997a6c497af4aad3a48
odnols <gustavobrandao31@outlook.com>
2023-03-16 21:14:31 -0300

    teste de sincronização

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 
 <head>
     <title>Um teste de conceito</title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-    <h1>Teste</h1>
+    <div class="banner_topo">
+        <h1>Teste</h1>
+    </div>
 
     <?php echo "aaaaaaaaaaaaa"; ?>
 </body>
