7814c218896fdfe6e0352f414e4ab1dab3a95049
odnols <gustavobrandao31@outlook.com>
2024-08-01 22:56:06 -0300

    Ajustes

--- kovarex.sql
+++ kovarex.sql
@@ -1,14 +1,19 @@
 create database kovarex;
-use kovarex;
 
-select * from usuario;
+use kovarex;
 
 create table usuario(
     id int not null auto_increment primary key,
     hierarquia int not null,
     nome varchar(255),
-    email varchar(100),
-    psw varchar(25)
+    email varchar(5000),
+    psw varchar(5000)
+) engine = InnoDB;
+
+create table empresa(
+    id int not null auto_increment primary key,
+    razao varchar(255),
+    cnpj varchar(18)
 ) engine = InnoDB;
 
 create table unidade(
@@ -31,12 +36,25 @@ create table pregao(
     termino date
 ) engine = InnoDB;
 
-create table itens_pregao(
+create table grupo_pregao(
+    id int not null auto_increment primary key,
+    id_pregao int not null,
+    id_empresa int null,
+    foreign key (id_pregao) references pregao(id),
+    foreign key (id_empresa) references empresa(id)
+) engine = InnoDB;
+
+create table item_pregao(
     id int not null auto_increment primary key,
     id_item int not null,
     id_pregao int not null,
+    id_grupo int null,
+    id_empresa int null,
+    quantidade int not null,
+    foreign key (id_grupo) references grupo_pregao(id),
     foreign key (id_item) references item(id),
-    foreign key (id_pregao) references pregao(id)
+    foreign key (id_pregao) references pregao(id),
+    foreign key (id_empresa) references empresa(id)
 ) engine = InnoDB;
 
 create table pedido(
